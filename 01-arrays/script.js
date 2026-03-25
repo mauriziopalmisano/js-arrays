c3abfc4873aaa4ca22595474f625c50414ec8e40
Maurizio Palmisano <maurizio.palmisano1@gmail.com>
2026-03-25 16:26:09 +0100

    esercizio 7 arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -77,7 +77,14 @@ console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+let lewisIndex = null;
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Lewis') {
+    lewisIndex = i;
+    console.log(lewisIndex);
+  }
+}
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
