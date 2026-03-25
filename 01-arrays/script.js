8c0f25f9740e79c9a0030535cd3f85a6e1067791
Maurizio Palmisano <maurizio.palmisano1@gmail.com>
2026-03-25 15:40:48 +0100

    esercizio 1 arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,7 +10,14 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+let fourthTeacher = null;
+
+for (let i = 0; i < teachers.length; i++) {
+  if (i === 3) {
+    fourthTeacher = teachers[i];
+    console.log(fourthTeacher);
+  }
+}
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
