446169537d8e85e755671deef64988b1e1dd42f7
Maurizio Palmisano <maurizio.palmisano1@gmail.com>
2026-03-25 16:54:53 +0100

    esercizio 3 arrays bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -35,6 +35,14 @@ console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+for(let i = 0; i < teachers.length; i++){
+  if (teachers[i] === 'Ed') {
+    teachers.splice(i, 1);
+  }
+}
+console.log(teachers);
+
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = null;
